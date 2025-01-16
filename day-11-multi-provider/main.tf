060bd706f33db8d0de0350ca25d5cb0b15927582
Ashish Ingle <ingleashish1996@gmail.com>
2025-01-16 17:02:07 +0530

    day-11-multi-provider

---
+++ day-11-multi-provider/main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "test1" {
+    bucket = "hsaskasasjk"
+    provider = aws.devops
+     
+}
+resource "aws_s3_bucket" "test" {
+    bucket = "asjakjkajs"
+  
+}
---
+++ day-11-multi-provider/provider.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    profile = "ashish"
+    region = "us-east-1"
+    alias = "devops"
+  
+}
+provider "aws" {
+    profile = "ashish"
+    region = "ap-south-1"
+  
+}
